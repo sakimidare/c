0c37ad2e4d3205203bdc48f39bb297449e056038
SakiMidare <ssmzhn@163.com>
2026-02-23 17:34:31 +0800

    added build.sh

---
+++ build.sh
@@ -0,0 +1,4 @@
+#！/bin/bash
+MDBOOK_VERSION="0.5.2"
+curl -sSL https://github.com/rust-lang/mdBook/releases/download/v$MDBOOK_VERSION/mdbook-v$MDBOOK_VERSION-x86_64-unknown-linux-gnu.tar.gz | tar -xz
+./mdbook build
